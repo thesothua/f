cd171d37ffea408c18296007b22d22eac72c89cc
Praveen Suthar <praveensuthar9553@gmail.com>
2026-08-08 02:38:08 +0530

    chore: update public disk configuration to use env-based media root and app URL

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,13 +38,20 @@ return [
             'report' => false,
         ],
 
+        // 'public' => [
+        //     'driver' => 'local',
+        //     'root' => storage_path('app/public'),
+        //     'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
+        //     'visibility' => 'public',
+        //     'throw' => false,
+        //     'report' => false,
+        // ],
+
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
+            'root' => env('MEDIA_ROOT', storage_path('app/public')),
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
-            'throw' => false,
-            'report' => false,
         ],
 
         's3' => [
