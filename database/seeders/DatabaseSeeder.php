968a6a3005106f9e1af843ca486fa396abd6ecc7
Praveen Suthar <praveensuthar9553@gmail.com>
2026-08-14 00:59:08 +0530

    feat: register WishlistItemSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             CampaignSeeder::class,
             BlogSeeder::class,
             SettingSeeder::class,
+            WishlistItemSeeder::class,
         ]);
     }
 }
